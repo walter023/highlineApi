2d0e3551f0bb90d81ac9e1223ce79744c48f84ac
waltermunoz <walter.munoz@gmail.com>
2019-04-12 09:01:06 +1000

    added subschema anchors and enums

---
+++ src/models/anchorSchema.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import anchorType from '../utilities/enum';
+
+
+const anchorSchema = mongoose.Schema({
+    rhs: [
+        mongoose.Schema({
+            main: { type: String, enum: anchorType, trim: true },
+            howManyOnMain: Number,
+            backup: { type: String, enum: anchorType, trim: true },
+            howMany: Number,
+            size: String
+        }, { _id: false })
+    ],
+    lhs: [
+        mongoose.Schema({
+            main: { type: String, enum: anchorType, trim: true },
+            howManyOnMain: Number,
+            backup: { type: String, enum: anchorType, trim: true },
+            howMany: Number,
+            size: String
+        }, { _id: false })
+    ]
+}, { _id: false });
+
+export default anchorSchema;
